e04c80d340de6a84ee39bc05d933cb8db8389c14
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2022-12-06 22:15:57 -0500

    create `say-hi` method; add controller-endpoint url value;

--- src/main/java/controllers/myPublicAPIController.java
+++ src/main/java/controllers/myPublicAPIController.java
@@ -1,4 +1,17 @@
 package controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller(value = "/public-api-controller")
 public class myPublicAPIController {
+
+    @GetMapping(path = "/say-hi")
+    public ResponseEntity sayHi() {
+        String spokenMessage = "Hello!";
+        ResponseEntity responseEntity = new ResponseEntity<>(spokenMessage, HttpStatus.OK);
+        return responseEntity;
+    }
 }
