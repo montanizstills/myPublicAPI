ea6b9e27322ef942ac53a7a5e043d1e9fccac887
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2022-12-27 16:43:03 -0500

    add static site `chatgpt.html`; update `myPublicAPIController.loadPage()` to use static `chatgpt.html` instead of eager loading; ensure POST request working as desired;

--- src/main/java/com/github/nez/controllers/myPublicAPIController.java
+++ src/main/java/com/github/nez/controllers/myPublicAPIController.java
@@ -1,8 +1,15 @@
 package com.github.nez.controllers;
 
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController()
 @RequestMapping(value = "/public-api-controller")
@@ -14,24 +21,26 @@ public class myPublicAPIController {
         ResponseEntity responseEntity = new ResponseEntity<>(spokenMessage, HttpStatus.OK);
         return responseEntity;
     }
+
+    @SneakyThrows
     @GetMapping(path = "/chatgpt")
-    public ResponseEntity loadPage(){
-        return new ResponseEntity<String>("<form>\n" +
-                "  <textarea id=\"text-input\"></textarea>\n" +
-                "  <button type=\"submit\">Submit</button>\n" +
-                "</form><script>const form = document.querySelector('form');\n" +
-                "form.addEventListener('submit', handleSubmit);function handleSubmit(event) {\n" +
-                "  event.preventDefault();\n" +
-                "\n" +
-                "  const textInput = document.querySelector('#text-input');\n" +
-                "  const text = textInput.value;\n" +
-                "\n" +
-                "  console.log(text);\n" +
-                "}</script>",HttpStatus.OK);
+    public ResponseEntity loadPage() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL fileUrl = classLoader.getResource("static/chatgpt.html");
+        if (fileUrl == null) {
+            // Return a NOT_FOUND response if the file was not found
+            return new ResponseEntity<>("cannot find/load file!",HttpStatus.NOT_FOUND);
+        }
+        // Read the html file from the URL
+        Path filePath = Paths.get(fileUrl.toURI());
+        String html = new String(Files.readAllBytes(filePath), StandardCharsets.UTF_8);
+        return new ResponseEntity<>(html, HttpStatus.OK);
     }
+
     @PostMapping(path = "/chatgpt", params = {})
-    public ResponseEntity postToModel(@PathVariable String... myEndpointParams){
+    public void postToModel() {
 //        new OpenAIClient(System.getenv("openapi-key"),"").makeRequest(CompletionRequest.class, myEndpointParams);
-        return null;
+//        return new ResponseEntity<>("You have posted!",HttpStatus.OK);
+        System.out.println("You have posted!");
     }
 }
