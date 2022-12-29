b03369f0d0373aba3a1368de03603468ed7126bf
Montaniz.Stills <montanizstills@gmail.com>
2022-12-29 02:22:40 -0500

    add endpoint to return `CompletionRequest.java` fields; update static page to create inputs for each field when selected;

--- src/main/java/com/github/nez/controllers/myPublicAPIController.java
+++ src/main/java/com/github/nez/controllers/myPublicAPIController.java
@@ -1,15 +1,19 @@
 package com.github.nez.controllers;
 
+import com.github.nez.models.requests.CompletionRequest;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController()
 @RequestMapping(value = "/public-api-controller")
@@ -29,7 +33,7 @@ public class myPublicAPIController {
         URL fileUrl = classLoader.getResource("static/chatgpt.html");
         if (fileUrl == null) {
             // Return a NOT_FOUND response if the file was not found
-            return new ResponseEntity<>("cannot find/load file!",HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("cannot find/load file!", HttpStatus.NOT_FOUND);
         }
         // Read the html file from the URL
         Path filePath = Paths.get(fileUrl.toURI());
@@ -41,5 +45,16 @@ public class myPublicAPIController {
     public void postToModel(@RequestBody String params) {
 //        new OpenAIClient("","").makeRequest(CompletionRequest.builder().build(), params);
         System.out.println(params.toString());
+    }
+
+    @GetMapping(path = "completion-request-fields")
+    public ResponseEntity getCompletionRequestFields() {
+        List fields = new ArrayList();
+        for (Field declaredField : CompletionRequest.class.getDeclaredFields()) {
+            // fancy code to remove the synthetic fields from the `field` name
+            String cleanedFieldName = declaredField.getName().substring(declaredField.getName().indexOf("this$") + 1);
+            fields.add(cleanedFieldName);
+        }
+        return new ResponseEntity<>(fields.toString(), HttpStatus.OK);
     }
 }
